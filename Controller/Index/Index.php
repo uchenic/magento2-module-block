8ca63434384a57c81e0f963423774132b678b205
sewwwer <www.svr@gmail.com>
2015-11-02 10:46:26 +0200

    add changes

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,7 +4,7 @@
  * See COPYING.txt for license details.
  */
 
-namespace Magento\SampleNewPage\Controller\Index;
+namespace Magento\SampleBlock\Controller\Index;
 
 /**
  * Responsible for loading page content.
