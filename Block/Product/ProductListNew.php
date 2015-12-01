6ebb74e5ad6fb377776ff1798270d235bc6d0fdc
sewwwer <www.svr@gmail.com>
2015-12-01 17:23:59 +0200

    fix widget issue

---
+++ Block/Product/ProductListNew.php
@@ -0,0 +1,32 @@
+<?php
+namespace Magento\SampleBlock\Block\Product;
+use Magento\CatalogWidget\Block\Product\ProductsList as AnotherClass;
+/**
+* 
+*/
+class ProductListNew extends AnotherClass implements \Magento\Widget\Block\BlockInterface
+{
+	
+	public function __construct(
+        \Magento\Catalog\Block\Product\Context $context,
+        \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
+        \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
+        \Magento\Framework\App\Http\Context $httpContext,
+        \Magento\Rule\Model\Condition\Sql\Builder $sqlBuilder,
+        \Magento\CatalogWidget\Model\Rule $rule,
+        \Magento\Widget\Helper\Conditions $conditionsHelper,
+        array $data = []
+    ) {
+        
+        parent::__construct(
+            $context,
+            $productCollectionFactory,
+            $catalogProductVisibility,
+            $httpContext,
+            $sqlBuilder,
+            $rule,
+            $conditionsHelper,
+            $data
+        );
+    }
+}
